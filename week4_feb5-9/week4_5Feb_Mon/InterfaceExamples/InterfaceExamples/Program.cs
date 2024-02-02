c9e7d80d988f1aeabef72616d277066bf632d1bd
Julie <juliet@ksu.edu>
2024-02-02 13:52:11 -0600

    notes shell before mon feb 5

---
+++ week4_feb5-9/week4_5Feb_Mon/InterfaceExamples/InterfaceExamples/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterfaceExamples;
+
+Console.WriteLine("Hello, World!");
+
+SortedSet<Dog> dogs = new SortedSet<Dog>{new Dog(), new Dog()};
+
+foreach (Dog d in dogs) Console.WriteLine(d); //crashes now
