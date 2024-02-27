9d83e9846f227afeb9885890883ab55d41e178a1
Julie <juliet@ksu.edu>
2024-02-27 16:27:44 -0600

    notes shell before wed feb 28

---
+++ week7_feb26-march1/ItemsExample/ItemsExample/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemsExample
+{
+    public class Person
+    {
+        public string Name { get; }
+
+        public Person(string n)
+        {
+            Name = n;
+        }
+    }
+}
