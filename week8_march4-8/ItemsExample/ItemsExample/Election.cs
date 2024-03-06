6f1ff0c9fb59703f4f51b72c01791e3855a69804
Julie <juliet@ksu.edu>
2024-03-06 15:06:56 -0600

    after wed march 6

--- week8_march4-8/ItemsExample/ItemsExample/Election.cs
+++ week8_march4-8/ItemsExample/ItemsExample/Election.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ItemsExample
 {
-    public class Election : ICollection<Candidate>, INotifyCollectionChanged
+    public class Election : ICollection<Candidate>, INotifyCollectionChanged, INotifyPropertyChanged
     {
         private List<Candidate> _candidates = new();
 
@@ -32,13 +32,41 @@ namespace ItemsExample
 
         public int Count => _candidates.Count;
 
+        public uint TotalVotes
+        {
+            get
+            {
+                uint total = 0;
+                foreach (Candidate c in _candidates)
+                {
+                    total += c.Votes;
+                }
+
+                return total;
+            }
+        }
+
         public bool IsReadOnly => false;
 
         public event NotifyCollectionChangedEventHandler? CollectionChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //I want this method to run whenever a candidate's votes change
+        private void HandleVoteChange(object? sender, PropertyChangedEventArgs e)
+        {
+
+            //this is called because a candidate's votes changed
+            //so I announce that my TotalVotes changed
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TotalVotes)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Winner)));
+        }
 
         public void Add(Candidate item)
         {
             _candidates.Add(item);
+
+            //listen to changes in item
+            item.PropertyChanged += HandleVoteChange;
 
             //want to invoke CollectionChanged
             CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item));
